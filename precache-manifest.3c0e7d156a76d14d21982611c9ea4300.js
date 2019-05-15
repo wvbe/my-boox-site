d0d7a71088a03ce66fe2e61e6da059fa1e173916
wvbe <wybe@x-54.com>
2019-05-16 01:40:12 +0200

    Updates

---
+++ precache-manifest.3c0e7d156a76d14d21982611c9ea4300.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600063945ae47ccc1946f4a5647e89eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d420f1af9cac0cf40a8",
+    "url": "/static/js/2.44bd544c.chunk.js"
+  },
+  {
+    "revision": "ae67321d30a7bdf5b2bb",
+    "url": "/static/js/main.f764e7e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb875cd0793165b36fbfea3bea661a30",
+    "url": "/static/media/Myboox_logo_800.eb875cd0.png"
+  },
+  {
+    "revision": "2ecb426f85ffc1c53b677556210e629f",
+    "url": "/static/media/fira-bold.2ecb426f.woff2"
+  },
+  {
+    "revision": "b39aa336eef260975654fde0959de6fe",
+    "url": "/static/media/fira-light.b39aa336.woff2"
+  },
+  {
+    "revision": "78773521b0ffe376bc7edd8ec2a591fb",
+    "url": "/static/media/fira-normal.78773521.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
